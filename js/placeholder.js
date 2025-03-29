1f54eebe860c74373670753c455f05b558fb11ae
Jeido <tourais.q@gmail.com>
2025-03-30 00:01:54 +0100

    Restore "Create Placeholder" functionality on dnd4.3+

--- js/placeholder.js
+++ js/placeholder.js
@@ -19,29 +19,30 @@ export class PlaceholderCombatant {
             }
         });
 
-        Hooks.on("getCombatTrackerEntryContext", (html, menu) => {
-            let idx = menu.findIndex(m => m.name == "COMBAT.CombatantUpdate") || 1;
-            menu.splice(idx, 0,
-                {
-                    name: i18n("MonksCombatDetails.CreatePlaceholder"),
-                    icon: '<i class="fas fa-user"></i>',
-                    condition: li => {
-                        let combatant = game.combats.viewed.combatants.get(li.data("combatant-id"));
-                        return combatant && !combatant.getFlag("monks-combat-details", "placeholder");
-                    },
-                    callback: li => {
-                        let combatant = game.combats.viewed.combatants.get(li.data("combatant-id"));
-                        if (combatant) {
-                            let combatantData = combatant.toObject();
-                            combatant = new Combatant(combatantData);
-                            delete combatant._id;
-                            if (combatant.initiative)
-                                combatant.initiative = combatant.initiative - 1;
-                            combatant.name = combatant.name + " [Placeholder]"
-                            new PlaceholderCombatantConfig(combatant).render(true);
-                        }
-                    }
-                });
+        Hooks.on("renderCombatTracker", (app, html, data) => {
+            if (!game.user.isGM) return;
+        
+            html.find(".combatant").each((_, element) => {
+                const combatantId = element.dataset.combatantId;
+                const combatant = game.combats.viewed.combatants.get(combatantId);
+        
+                if (combatant && !combatant.getFlag("monks-combat-details", "placeholder")) {
+                    $($('<a>')
+                        .addClass('combatant-control')
+                        .attr('data-tooltip', i18n("MonksCombatDetails.CreatePlaceholder"))
+                        .html('<i class="fas fa-user-plus"></i>')
+                        .on("click", () => {
+                            const combatantData = combatant.toObject();
+                            delete combatantData._id;
+                            combatantData.name = combatant.name + " [Placeholder]";
+                            combatantData.initiative = combatant.initiative ? combatant.initiative - 1 : null;
+        
+                            new PlaceholderCombatantConfig(new Combatant(combatantData)).render(true);
+                }).insertBefore($('.token-effects', this)));
+                }
+                
+            })
+    
         });
 
         let CombatantInitiative = function (wrapped, ...args) {
